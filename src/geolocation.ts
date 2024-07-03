d1a57acbfebb6c792e64255319da2fb20215acf2
Guillaume Quittet <guillaume.quittet@gmail.com>
2024-07-03 22:30:41 +0200

    feat(geolocation): replace node-geocoder with builtin nominatim call
    Remove node-geocoder because of https://github.com/nchaulet/node-geocoder/issues/357

--- src/geolocation.ts
+++ src/geolocation.ts
@@ -1,14 +1,63 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
-import nodeGeoCoder from "node-geocoder";
 
-const geocoder = nodeGeoCoder({
-  provider: "openstreetmap",
+// @ts-expect-error because axios-retry use ESM and does not declare correctly the default export in package.json
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay() {
+    return 1000;
+  },
 });
 
+export type OpenStreetMapResult = {
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  lat: string;
+  lon: string;
+  display_name: string;
+  class: string;
+  type: string;
+  importance: number;
+  icon: string;
+  place_rank: number;
+  addresstype: string;
+  name: string;
+  address: {
+    road: string;
+    // cSpell:disable
+    neighbourhood: string;
+    suburb: string;
+    city: string;
+    municipality: string;
+    county: string;
+    state: string;
+    region: string;
+    postcode: string;
+    country: string;
+    country_code: string;
+  };
+  boundingbox: string[];
+};
+
 export const localize = async (address: string) => {
-  const result = await geocoder.geocode(address);
-  return result[0]!;
+  const url = new URL("https://nominatim.openstreetmap.org/search");
+  url.searchParams.append("addressdetails", "1");
+  url.searchParams.append("q", address);
+  url.searchParams.append("format", "json");
+
+  const { data } = await axios.get<OpenStreetMapResult[]>(url.toString());
+
+  if (data.length === 0 || !data[0]) {
+    // cSpell:disable
+    throw new Error("Address not found using Nominatim OpenStreetMap API.");
+  }
+
+  return {
+    longitude: Number.parseInt(data[0].lon, 10),
+    latitude: Number.parseInt(data[0].lat, 10),
+  };
 };
 
 type Route = {
@@ -21,9 +70,6 @@ type Route = {
     distance: number;
   }>;
 };
-
-// @ts-expect-error because axios-retry use ESM and does not declare correctly the default export in package.json
-axiosRetry(axios, { retries: 3 });
 
 export const computeWeight = async (
   start: { latitude: number; longitude: number },
